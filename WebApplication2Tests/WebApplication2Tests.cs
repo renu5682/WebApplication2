a5768d394580bf9ff949dfd39736c8ef2b03a0ea
renu5682 <renu.ap@gmail.com>
2015-08-27 23:40:06 -0700

    TEST1

---
+++ WebApplication2Tests/WebApplication2Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WebApplication2;
+using NUnit.Framework;
+namespace WebApplication2Tests
+{
+    [TestFixture]
+    public class WebApplication2Tests
+    {
+        [Test]
+        public void EquilateralTriangleTest()
+        {            
+            Assert.AreEqual(TriangleType.Equilateral, TriangleTester.GetTriangleType(1,1,1));
+        }
+    }
+}
